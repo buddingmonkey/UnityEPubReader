b2e5ba0c6c110c187647c840b0ab13bfffd69988
Andrew Eiche <andrew@northstargames.com>
2015-09-22 16:12:53 -0400

    Initial commit

---
+++ UnityEPubReader/Assets/Scripts/EbookRenderer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UEPub;
+
+public class EbookRenderer : MonoBehaviour {
+	public Text displayText;
+	public TextAsset testBook;
+
+	//private EpubBook epubBook;
+
+	// Use this for initialization
+	void Start () {
+		OpenEbookFile ();
+	}
+	
+	void OpenEbookFile(){
+		// Opening a book
+		//epubBook = EpubReader.OpenBook("austen-pride-and-prejudice-illustrations.epub");
+		//EpubChapter chapter = epubBook.Chapters [0];
+		//displayText.text = chapter.HtmlContent;
+		var epub = new UEPubReader ("Assets/Books/austen-pride-and-prejudice-illustrations.epub");
+		Debug.Log (epub.epubFolderLocation);
+	}
+}
---
+++ UnityEPubReader/Assets/Scripts/UEPubReader.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using System.IO;
+using System.Text;
+
+namespace UEPub{
+	public class UEPubReader {
+
+		public string epubFolderLocation { get; private set; }
+
+		public Dictionary<string, string> chapters;
+
+		private string opfFileString;
+
+		public UEPubReader(string file){
+			var fileArray = file.Split('.');
+
+			if (fileArray [fileArray.Length - 1].ToLower() != "epub") {
+				Debug.LogErrorFormat ("The file {0} is not a .epub file", file);
+				return;
+			}
+
+			fileArray = string.Join (".", fileArray, 0, fileArray.Length - 1).Split (System.IO.Path.DirectorySeparatorChar);
+			var folderName = fileArray[fileArray.Length - 1];
+
+			epubFolderLocation = Application.temporaryCachePath + "/" + folderName;
+
+			ZipUtil.Unzip ( file, epubFolderLocation);
+		}
+
+		private void ParseContainer(){
+			var containerFile = epubFolderLocation + "/META-INF/container.xml";
+
+			XmlDocument doc = new XmlDocument ();
+			doc.Load (containerFile);
+
+			XmlNode rootFile =
+				doc.SelectSingleNode("/container/rootfiles/rootfile");
+			opfFileString = rootFile.Attributes ["full-path"].InnerText;
+		}
+	}
+}

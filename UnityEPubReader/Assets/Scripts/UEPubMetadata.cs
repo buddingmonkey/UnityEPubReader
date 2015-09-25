9b08b71ab837f092a0ac967b34999a83c09eabf3
Andrew Eiche <andrew@northstargames.com>
2015-09-25 12:44:06 -0400

    Now parsing metadata!!!

---
+++ UnityEPubReader/Assets/Scripts/UEPubMetadata.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System;
+
+namespace UEPub
+{
+	public class UEPubMetadata
+	{
+		// Required:
+		public string title;
+		public string language;
+		public string identifier;
+
+		// Optional:
+		public string creator;
+		public string contributor;
+		public string publisher;
+		public string subject;
+		public string description;
+		public DateTime date;
+		public string type;
+		public string format;
+		public string source;
+		public string relation;
+		public string coverage;
+		public string rights;
+	}
+}
--- UnityEPubReader/Assets/Scripts/UEPubReader.cs
+++ UnityEPubReader/Assets/Scripts/UEPubReader.cs
@@ -1,15 +1,17 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.Xml;
 using System.IO;
 using System.Text;
 using System.Linq;
+using System;
+using UnityEngine;
 
 namespace UEPub{
 	public class UEPubReader {
 
 		public string epubFolderLocation { get; private set; }
+		public UEPubMetadata metadata { get; private set; }
 
 		public List<string> chapters;
 
@@ -64,27 +66,68 @@ namespace UEPub{
 			xmlnsManager.AddNamespace("dc", "http://purl.org/dc/elements/1.1/");
 			xmlnsManager.AddNamespace("dcterms", "http://purl.org/dc/terms/");
 
+			ReadMetadata (doc, xmlnsManager);
+
+			ReadChapters (opfFile, doc, xmlnsManager);
+		}
+
+		private void ReadChapters (string opfFile, XmlDocument doc, XmlNamespaceManager xmlnsManager)
+		{
 			var nodes = doc.SelectNodes ("/ns:package/ns:manifest/ns:item", xmlnsManager);
-
 			foreach (XmlNode node in nodes) {
-				bookItems[node.Attributes["id"].InnerText] = node.Attributes["href"].InnerText;
+				bookItems [node.Attributes ["id"].InnerText] = node.Attributes ["href"].InnerText;
 			}
 
-			nodes = doc.SelectNodes("/ns:package/ns:spine/ns:itemref", xmlnsManager);
-
-			foreach (XmlNode node in nodes){
-				spine.Add(node.Attributes["idref"].InnerText);
+			nodes = doc.SelectNodes ("/ns:package/ns:spine/ns:itemref", xmlnsManager);
+			foreach (XmlNode node in nodes) {
+				spine.Add (node.Attributes ["idref"].InnerText);
 			}
 
-			var toc = spine.Select (x => bookItems [x])
-					.ToList ();
-
+			var toc = spine.Select (x => bookItems [x]).ToList ();
 			chapters = new List<string> ();
 
 			foreach (string s in toc) {
-				StreamReader sr = new StreamReader(Path.GetDirectoryName(opfFile) + "/" + s);
-				chapters.Add(sr.ReadToEnd());
-				sr.Close();
+				StreamReader sr = new StreamReader (Path.GetDirectoryName (opfFile) + "/" + s);
+				chapters.Add (sr.ReadToEnd ());
+				sr.Close ();
+			}
+		}
+
+		private void ReadMetadata(XmlDocument doc, XmlNamespaceManager xmlnsManager){
+			metadata = new UEPubMetadata ();
+
+			// Required properties
+			metadata.title = doc.SelectSingleNode ("/ns:package/ns:metadata/dc:title", xmlnsManager).InnerText;
+			metadata.language = doc.SelectSingleNode ("/ns:package/ns:metadata/dc:language", xmlnsManager).InnerText;
+			metadata.identifier = doc.SelectSingleNode ("/ns:package/ns:metadata/dc:identifier", xmlnsManager).InnerText;
+
+			// Optional props
+			SetMetadataProperty (ref metadata.creator, "/ns:package/ns:metadata/dc:creator", doc, xmlnsManager);
+			SetMetadataProperty (ref metadata.contributor, "/ns:package/ns:metadata/dc:contributor", doc, xmlnsManager);
+			SetMetadataProperty (ref metadata.coverage, "/ns:package/ns:metadata/dc:coverage", doc, xmlnsManager);
+			SetMetadataProperty (ref metadata.description, "/ns:package/ns:metadata/dc:description", doc, xmlnsManager);
+			SetMetadataProperty (ref metadata.format, "/ns:package/ns:metadata/dc:format", doc, xmlnsManager);
+			SetMetadataProperty (ref metadata.publisher, "/ns:package/ns:metadata/dc:publisher", doc, xmlnsManager);
+			SetMetadataProperty (ref metadata.relation, "/ns:package/ns:metadata/dc:relation", doc, xmlnsManager);
+			SetMetadataProperty (ref metadata.rights, "/ns:package/ns:metadata/dc:rights", doc, xmlnsManager);
+			SetMetadataProperty (ref metadata.source, "/ns:package/ns:metadata/dc:source", doc, xmlnsManager);
+			SetMetadataProperty (ref metadata.subject, "/ns:package/ns:metadata/dc:subject", doc, xmlnsManager);
+			SetMetadataProperty (ref metadata.type, "/ns:package/ns:metadata/dc:type", doc, xmlnsManager);
+
+			// Date
+
+			var node = doc.SelectSingleNode ("/ns:package/ns:metadata/dc:date", xmlnsManager);
+
+			if (node != null) {
+				metadata.date = DateTime.ParseExact(node.InnerText, "yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture);
+			}
+		}
+
+		private void SetMetadataProperty(ref string property, string XPath, XmlDocument doc, XmlNamespaceManager xmlnsManager){
+			var node = doc.SelectSingleNode (XPath, xmlnsManager);
+
+			if (node != null) {
+				property = node.InnerText;
 			}
 		}
 	}
